f132cc3e72e742f5168a9a039919054f88182d85
Anay04 <71800047+Anay04@users.noreply.github.com>
2021-01-31 13:00:53 -0500

    Add files via upload

---
+++ BaseClass.js
@@ -0,0 +1,23 @@
+class BaseClass {
+  constructor(x,y,w,h,r) {
+    var options = {
+      restitution : 0.3,
+        friction : 0.5,
+        density : 1.2,
+      }
+    this.x = x;
+    this.y = y;
+    this.r = r;
+    this.w = w;
+    this.h = h;
+    this.body = Bodies.rectangle(x, y, w, h, r, options);
+    World.add(world, this.body);
+  }
+  display(){
+    push();
+    translate(this.body.position.x, this.body.position.y);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.w, this.h);
+    pop();
+  }
+};
--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Project-28
-Project 28 Work
+# angryBirdsStage2StudentActivity
+Student Activity Link for Angry Birds Stage 2
